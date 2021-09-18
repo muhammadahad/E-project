3acb7518d1ea58153f52ef4607ec28d133cffbca
kashfa <kashfa@DESKTOP-C49OUMI>
2021-09-17 21:48:03 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using Ahad_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ahad_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProjectDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+
+        public IActionResult Timesheet()
+        {
+            return View();
+        }
+
+        public IActionResult Leaders()
+        {
+            return View();
+        }
+
+        public IActionResult TicketsView()
+        {
+            return View();
+        }
+
+        public IActionResult TicketDetail()
+        {
+            return View();
+        }
+
+        public IActionResult Clients()
+        {
+            return View();
+        }
+
+        public IActionResult ClientProfile()
+        {
+            return View();
+        }
+
+        public IActionResult Members()
+        {
+            return View();
+        }
+
+        public IActionResult MembersProfile()
+        {
+            return View();
+        }
+
+        public IActionResult Holidays()
+        {
+            return View();
+        }
+
+        public IActionResult AttendanceEmployees()
+        {
+            return View();
+        }
+
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+        public IActionResult LeaveRequest()
+        {
+            return View();
+        }
+        public IActionResult Department()
+        {
+            return View();
+        }
+
+        public IActionResult Invoices()
+        {
+            return View();
+        }
+
+        public IActionResult Payments()
+        {
+            return View();
+        }
+
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeSalary()
+        {
+            return View();
+        }
+
+        public IActionResult Calander()
+        {
+            return View();
+        }
+
+        public IActionResult ChatApp()
+        {
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
